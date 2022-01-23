0c37e7189d2b696dd24eb581be298e5caec83d87
Pαrαdox <72992708+KanekiWeb@users.noreply.github.com>
2022-01-23 22:54:42 +0100

    🐺 Optimise Webhook Logs
    🐺 Optimise Webhook Logs

--- Inc/fonctions.php
+++ Inc/fonctions.php
@@ -16,4 +16,17 @@
         return $req->rowCount();
     }
 
+    function SendToWebhook($webhook, $data) {
+        $ch = curl_init($webhook);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_exec($ch);
+        curl_close($ch);
+    }
+
 ?>
