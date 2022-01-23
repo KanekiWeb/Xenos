4062ccc146e90e4ef2eea2c673044f1d8502aee5
Pαrαdox <72992708+KanekiWeb@users.noreply.github.com>
2022-01-23 23:57:48 +0100

    🐺 Source Code
    🐺 Source Code

--- login.php
+++ login.php
@@ -4,45 +4,10 @@
     if(CheckLogin()){
         header('Location: ');
     }
+
+    $title = "Login";
+    require('Inc/Dash.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-    <!-- Icon -->
-    <link rel="shortcut icon" type="image/jpg" href="assets/images/logo.jpg"/>
-    
-    <!-- Reset Meta Tags -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Title -->
-    <title>Xenos Login</title>
-
-    <!-- Css -->
-    <link rel="stylesheet" href="assets/css/style.css">
-
-    <!-- Javascript -->
-    <script src="assets/js/d3.v3.min.js" charset="utf-8"></script>
-</head>
-<body>
-
-    <!-- Navigation Bar -->
-    <header>
-        <nav>
-            <a target="_blank" href="https://github.com/KanekiWeb" class="header_logo">
-                <span>Xenos</span>
-            </a>
-            <div class="header_links">
-                <ul class="nav_links">
-                    <li class="nav_link"><a href="#">Acceuil</a></li>
-                    <li class="nav_link"><a href="tokens">Tokens</a></li>
-                </ul>
-            </div>
-        </nav>
-    </header>
 
     <!-- Main -->
     <section class="section_main">
